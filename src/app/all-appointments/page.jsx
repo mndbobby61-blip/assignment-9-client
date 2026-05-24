d067bb7257818fd15f650580c26ad20b0af0eb3d
MdBobby <mndbobby61@gmail.com>
2026-05-25 05:01:50 +0600

    search bar add

--- src/app/all-appointments/page.jsx
+++ src/app/all-appointments/page.jsx
@@ -3,11 +3,14 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { authClient } from "@/lib/auth-client";
+import { useState } from "react"; 
 
 import doctors from "@/data/doctors";
 import DoctorCard from "../components/DoctorCard";
 
 export default function AllAppointments() {
+
+  const [search, setSearch] = useState("");
 
   const router = useRouter();
 
@@ -27,6 +30,11 @@ export default function AllAppointments() {
     checkUser();
 
   }, [router]);
+  const filteredDoctors = doctors.filter((doctor) =>
+    doctor.name.toLowerCase().includes(search.toLowerCase()) ||
+    doctor.specialty.toLowerCase().includes(search.toLowerCase()) ||
+    doctor.hospital.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <section className="max-w-7xl mx-auto px-4 py-20">
@@ -40,10 +48,19 @@ export default function AllAppointments() {
           Browse all available appointments
         </p>
       </div>
+      <form className="mt-6 flex justify-center" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Search doctor, specialty..."
+          className="w-full md:w-1/2 px-4 py-3 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mt-14">
 
-        {doctors.map((doctor) => (
+        {filteredDoctors.map((doctor) => (
           <DoctorCard key={doctor.id} doctor={doctor} />
         ))}
 
